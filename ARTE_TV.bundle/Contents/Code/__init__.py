1c09e64abf7321572986a0b3eb4876c8d105c13e
FraKtus <fraktus@arkaos.net>
2015-11-02 07:25:24 +0100

    Fix for web site format changes November 1st

--- ARTE_TV.bundle/Contents/Code/__init__.py
+++ ARTE_TV.bundle/Contents/Code/__init__.py
@@ -1,6 +1,6 @@
 import re, string
-ARTE_CONCERT_URL	  = 'http://concert.arte.tv'
-ARTE_URL	  = 'http://www.arte.tv'
+ARTE_CONCERT_URL  = 'http://concert.arte.tv'
+ARTE_URL  = 'http://www.arte.tv'
 
 ICON = 'arte_logo.png'
 ART = 'art-default.png'
@@ -24,9 +24,11 @@ def MainMenu():
   oc = ObjectContainer(
     objects = [
       DirectoryObject(key = Callback(GetItemList, url='/fr', title2='Concerts French'), title = L('Arte Concerts French')),
-      DirectoryObject(key = Callback(GetPlus7ItemList, url='/guide/fr/plus7.json', title2='Arte +7 French'), title = L('Arte +7 French')),
+      # DirectoryObject(key = Callback(GetPlus7ItemList, url='/guide/fr/plus7.json', title2='Arte +7 French'), title = L('Arte +7 French')),
+      DirectoryObject(key = Callback(GetPlus7ItemList, url='/guide/fr/plus7/?zone=europe-de-fr', title2='Arte +7 French'), title = L('Arte +7 French')),
       DirectoryObject(key = Callback(GetItemList, url='/de', title2='Concerts German'), title = L('Arte Concerts German')),
-      DirectoryObject(key = Callback(GetPlus7ItemList, url='/guide/de/plus7.json', title2='Arte +7 German'), title = L('Arte +7 German'))
+      # DirectoryObject(key = Callback(GetPlus7ItemList, url='/guide/de/plus7.json', title2='Arte +7 German'), title = L('Arte +7 German'))
+      DirectoryObject(key = Callback(GetPlus7ItemList, url='/guide/de/plus7/?zone=europe-de-fr', title2='Arte +7 German'), title = L('Arte +7 German'))
     ]
   )                                 
   # append programs list directly
@@ -34,7 +36,7 @@ def MainMenu():
   return oc
 
 ####################################################################################################
-	
+
 def GetItemList(url, title2, page=''):
   Log ("ARTE GetItemList :" + url)
   Log.Exception('GetItemList')
@@ -76,8 +78,8 @@ def GetPlus7Param (video, param, remove_slash, remove_spaces):
   return result
   
 ####################################################################################################
-	
-def GetPlus7ItemList(url, title2, page=''):
+
+def GetPlus7ItemListOld(url, title2, page=''):
   Log ("ARTE GetPlus7ItemList :" + url)
   Log.Exception('GetPlus7ItemList')
   oc = ObjectContainer(title2=title2, view_group='InfoList')
@@ -101,3 +103,42 @@ def GetPlus7ItemList(url, title2, page=''):
       pass    
   return oc
 
+####################################################################################################
+
+def GetPlus7ItemList(url, title2, page=''):
+  Log ("ARTE GetPlus7ItemList :" + url)
+  Log.Exception('GetPlus7ItemList')
+  oc = ObjectContainer(title2=title2, view_group='InfoList')
+  Log.Exception('videos')
+  program_url = ARTE_URL + url
+  Log ("ARTE +7 url : " + program_url)
+  html = HTML.ElementFromURL(program_url)
+  scripts = html.xpath('//script')
+  for script in scripts:
+    try:
+      js = HTML.StringFromElement(script)
+      # Log ("js: " + js)
+      js = js.split("js/page/home")[1]
+      # Log ("found script: " + js)
+      video_groups = js.split("videos")
+      for video_group in video_groups:
+        videos = video_group.split("{")
+        for video in videos:
+          try:
+            Log ("Test Video : " + video)
+            video_page_url = GetPlus7Param (video, "\"url\"", 1, 1)
+            Log ("video url: " + video_page_url)
+            title = GetPlus7Param (video, "\"title\"", 0, 0).decode("unicode_escape")
+            Log ("title: " + title)
+            img = GetPlus7Param (video, "\"thumbnail_url\"", 1, 1)
+            Log ("img: " + img)
+            oc.add(VideoClipObject(url = video_page_url, title = title, thumb=img))
+              
+          except:
+            pass
+
+    except:
+      Log.Exception("error adding VideoClipObject")
+      pass    
+  
+  return oc
